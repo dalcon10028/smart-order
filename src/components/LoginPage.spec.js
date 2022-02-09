6e8b0710a9f54d56a93892ad994e28575bd20c35
dalcon <dalcon10280@gmail.com>
2022-02-09 16:56:43 +0900

    test: 아이디, 비밀번호 실시간 반영 테스트 작성

--- src/components/LoginPage.spec.js
+++ src/components/LoginPage.spec.js
@@ -1,19 +1,33 @@
 import { mount } from '@vue/test-utils';
 import LoginPage from './LoginPage.vue';
+import LoginForm from './LoginForm.vue';
+import UserInfoView from './UserInfoView.vue';
 
 describe('LoginPage.vue', () => {
-  it('컴포넌트가 마운트되면 LoginForm 컴포넌트와 UserInfoView 컴포넌트가 렌더링 되어야한다.', () => {
+  it('컴포넌트가 마운트되면 LoginForm 컴포넌트와 UserInfoView 컴포넌트가 렌더링 되어야합니다.', () => {
     // given
     const wrapper = mount(LoginPage);
 
-    // when
+    // then
+    expect(wrapper.contains(LoginForm)).toBe(true);
+    expect(wrapper.contains(UserInfoView)).toBe(true);
+  });
+
+  it('로그인 버튼 하단에는 아이디와 비밀번호가 실시간으로 반영이 되어 표시되어야 합니다.', () => {
+    // given
+    const wrapper = mount(LoginPage);
     const idInput = wrapper.find('#id');
     const pwInput = wrapper.find('#pw');
-    const userInfo = wrapper.find('#user-info');
+    const idView = wrapper.find('#id-view');
+    const pwView = wrapper.find('#pw-view');
+    const [mockId, mockPw] = ['yeonkwon', '1028'];
+
+    // when
+    idInput.setValue(mockId);
+    pwInput.setValue(mockPw);
 
     // then
-    expect(idInput).toBeTruthy();
-    expect(pwInput).toBeTruthy();
-    expect(userInfo).toBeTruthy();
+    expect(idView.text()).toBe(`아이디: ${mockId}`);
+    expect(pwView.text()).toBe(`비밀번호: ${mockPw}`);
   });
 });
