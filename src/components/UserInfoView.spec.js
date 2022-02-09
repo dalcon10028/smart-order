aef583020c2dc3f4a62e4db7c2cf5bb1b4c580ad
dalcon <dalcon10280@gmail.com>
2022-02-09 16:41:09 +0900

    test: UserInfoView 컴포넌트 테스트 작성

---
+++ src/components/UserInfoView.spec.js
@@ -0,0 +1,13 @@
+import { shallowMount } from '@vue/test-utils';
+import UserInfoView from './UserInfoView.vue';
+
+describe('UserInfoView.vue', () => {
+  it('유저 정보를 보여주는 #user-info-view가 생성됩니다.', () => {
+    // given
+    const wrapper = shallowMount(UserInfoView);
+    const userInfoView = wrapper.find('#user-info-view');
+
+    // then
+    expect(userInfoView).toBeTruthy();
+  });
+});
