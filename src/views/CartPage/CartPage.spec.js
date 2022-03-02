ccb3418dddfe612d97a30c2ff1077015075b7f9d
dalcon <dalcon10280@gmail.com>
2022-03-02 11:43:14 +0900

    test: beforeEach를 사용하여 중복코드 제거

--- src/views/CartPage/CartPage.spec.js
+++ src/views/CartPage/CartPage.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount, mount, flushPromises } from '@vue/test-utils';
+import { shallowMount, flushPromises } from '@vue/test-utils';
 import { fetchCart } from '@/api';
 import { displayPrice, optionsFormat } from '@/utils/format';
 import CartPage from './CartPage.vue';
@@ -16,66 +16,53 @@ const totalPrice = product => {
 };
 
 describe('CartPage.vue', () => {
-  it('상품의 사진이 표시됩니다.', async () => {
-    const productList = await fetchCart();
-    const wrapper = shallowMount(CartPage);
+  let productList;
+  let wrapper;
+
+  beforeEach(async () => {
+    productList = await fetchCart();
+    wrapper = shallowMount(CartPage);
     await flushPromises();
+  });
+
+  it('상품의 사진이 표시됩니다.', () => {
     const firstProductImage = wrapper.find('[data-test="product-image"]');
 
     expect(firstProductImage.attributes('src')).toEqual(productList[0].imageUrl);
     expect(firstProductImage.attributes('alt')).toEqual(productList[0].name);
   });
 
-  it('상품의 한글 상품명이 표시됩니다.', async () => {
-    const productList = await fetchCart();
-    const wrapper = shallowMount(CartPage);
-    await flushPromises();
+  it('상품의 한글 상품명이 표시됩니다.', () => {
     const firstProductName = wrapper.find('[data-test="product-name"]');
 
     expect(firstProductName.text()).toEqual(productList[0].name);
   });
 
-  it('상품의 영문 상품명이 표시됩니다.', async () => {
-    const productList = await fetchCart();
-    const wrapper = shallowMount(CartPage);
-
-    await flushPromises();
+  it('상품의 영문 상품명이 표시됩니다.', () => {
     const firstProductEngName = wrapper.find('[data-test="product-eng-name"]');
 
     expect(firstProductEngName.text()).toEqual(productList[0].engName);
   });
 
-  it('옵션 내역이 표시됩니다.', async () => {
-    const productList = await fetchCart();
-    const wrapper = shallowMount(CartPage);
-    await flushPromises();
+  it('옵션 내역이 표시됩니다.', () => {
     const firstProductOption = wrapper.find('[data-test="product-option-name"]');
 
     expect(firstProductOption.text()).toEqual(optionsFormat(productList[0].options));
   });
 
-  it('상품의 단일 가격이 표시됩니다.', async () => {
-    const productList = await fetchCart();
-    const wrapper = shallowMount(CartPage);
-    await flushPromises();
+  it('상품의 단일 가격이 표시됩니다.', () => {
     const firstProductPrice = wrapper.find('[data-test="product-price"]');
 
     expect(firstProductPrice.text()).toEqual(displayPrice(productList[0].price));
   });
 
-  it('퍼스널 옵션 이름이 표시됩니다.', async () => {
-    const productList = await fetchCart();
-    const wrapper = shallowMount(CartPage);
-    await flushPromises();
+  it('퍼스널 옵션 이름이 표시됩니다.', () => {
     const firstPersonalOptionName = wrapper.find('[data-test="personal-option-name"]');
 
     expect(firstPersonalOptionName.text()).toEqual(productList[0].options.personal[0].name);
   });
 
-  it('퍼스널 옵션 가격이 표시됩니다.', async () => {
-    const productList = await fetchCart();
-    const wrapper = shallowMount(CartPage);
-    await flushPromises();
+  it('퍼스널 옵션 가격이 표시됩니다.', () => {
     const firstPersonalOptionPrice = wrapper.find('[data-test="personal-option-price"]');
 
     expect(firstPersonalOptionPrice.text()).toEqual(
@@ -83,10 +70,7 @@ describe('CartPage.vue', () => {
     );
   });
 
-  it('상품의 합산 가격이 표시됩니다.', async () => {
-    const productList = await fetchCart();
-    const wrapper = shallowMount(CartPage);
-    await flushPromises();
+  it('상품의 합산 가격이 표시됩니다.', () => {
     const productTotalPrice = wrapper.find('[data-test="product-total-price"]');
 
     const {
@@ -102,9 +86,6 @@ describe('CartPage.vue', () => {
   });
 
   it('장바구니의 총액이 표시됩니다.', async () => {
-    const productList = await fetchCart();
-    const wrapper = mount(CartPage);
-    await flushPromises();
     const amountDue = wrapper.find('[data-test="amt-due"]');
 
     const mockAmountDue = productList.reduce((acc, product) => acc + totalPrice(product), 0);
